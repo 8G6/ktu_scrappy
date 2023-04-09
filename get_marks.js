e56a61f400f2f9272a8b3af0b5e3c99766f4eed8
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2023-04-09 23:59:35 +0530

    added fail logging

--- get_marks.js
+++ get_marks.js
@@ -2,9 +2,17 @@ const puppeteer       = require('C:\\Users\\The Ryu\\node_modules\\puppeteer-cor
 const config          = require('./config/config');
 const login    = require('./scripts/eval');
 const get_mark_json       = require('./scripts/details');
+let   {
+    type,
+    check_fail,
+    core_check,
+    stats
+}                         = require("./scripts/fail")
+
 const {writeFile}     = require('fs').promises
 
 async function getMarks() {
+
     const browser = await puppeteer.launch(config.options);
     const page = await browser.newPage();
     
@@ -25,11 +33,14 @@ async function getMarks() {
     });
     
    
-    let json = await page.evaluate(get_mark_json);
-    console.log(json)
+    let  grades = await page.evaluate(get_mark_json);
+
+    check_fail(grades)
     
+    console.log("_".repeat(10),"\n")
+
     console.log('Writing to file');
-    await writeFile('data.json',JSON.stringify(json))
+    await writeFile('data.json',JSON.stringify(grades))
     console.log('File written');
 
     await browser.close();
